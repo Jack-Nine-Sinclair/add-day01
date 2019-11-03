e839e774c67c99e72a9df7efcc1b629e0163385e
jack <15998220805@163.com>
2019-11-03 20:58:55 +0800

    new

---
+++ doMoreEx/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+class ReactX extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            list:["aa","bb"]
+         }
+    }
+    render() { 
+        return ( 
+            <div>
+                {
+                    
+                }
+            </div>
+         );
+    }
+}
+ 
+export default ReactX;
